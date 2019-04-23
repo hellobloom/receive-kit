4d6150a3650357361b2904a9d9fb1304117ba060
Eddie Hedges <eddiehedges@gmail.com>
2019-04-23 17:02:41 -0500

    Delete a ton of code that was ported over to share-kit

--- index.ts
+++ index.ts
@@ -1,24 +1,18 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-
 import express from "express";
-import { ResponseData, util as shareKitUtil } from "@bloomprotocol/share-kit";
-import { IVerifiedData } from "@bloomprotocol/share-kit/dist/src/types";
-import { TDecodedLog, getDecodedTxEventLogs } from "./txUtils";
-import { sortObject, isNullOrWhiteSpace } from "./utils";
-import {
-  validateRequestFormat,
-  validateBasicOffChainProperties,
-  validateOnChainProperties
-} from "./validation";
+import { validateUntypedResponseData } from "@bloomprotocol/share-kit";
 
 const app = express();
 const port = process.env.PORT;
 if (!port) {
   throw Error("Missing required PORT environment variable");
 }
-const provider = process.env.WEB3_PROVIDER;
-if (!provider) {
+const validateOnChain =
+  typeof process.env.VALIDATE_ON_CHAIN === "string" &&
+  process.env.VALIDATE_ON_CHAIN.toLowerCase() === "true";
+const web3Provider = process.env.WEB3_PROVIDER;
+if (validateOnChain && !web3Provider) {
   throw Error("Missing required WEB3_PROVIDER environment variable");
 }
 
@@ -28,101 +22,13 @@ app.use(express.json());
 app.post(
   "/api/receive",
   async (req: express.Request, res: express.Response) => {
-    // Ensure the structure of the JSON is formatted properly
-    const reqFormatValidation = validateRequestFormat(req);
-    if (reqFormatValidation.length) {
-      console.log(
-        `reqFormatValidation: ${JSON.stringify(reqFormatValidation)}`
-      );
-      return res.status(400).json({ errors: reqFormatValidation });
+    const output = await validateUntypedResponseData(req.body, {
+      validateOnChain,
+      web3Provider
+    });
+    if (output.errors && output.errors.length > 0) {
+      return res.status(400).json({ errors: output.errors });
     }
-
-    const shareKitResData: ResponseData = sortObject(req.body);
-    shareKitResData.data = shareKitResData.data.map(d => sortObject(d));
-
-    // Validate the integrity of basic off-chain properties (subject, packedData)
-    const basicOffChainValidation = validateBasicOffChainProperties(
-      shareKitResData
-    );
-    if (basicOffChainValidation.length) {
-      console.log(
-        `basicOffChainValidation: ${JSON.stringify(basicOffChainValidation)}`
-      );
-      return res.status(400).json({
-        errors: basicOffChainValidation
-      });
-    }
-
-    // Verify the off-chain data integrity of each data node
-    const offChainValidation = shareKitResData.data.map(d => ({
-      layer2Hash: d.layer2Hash,
-      errors: shareKitUtil.verifyOffChainDataIntegrity(d)
-    }));
-    const hasOffChainVerificationErrors = !offChainValidation.every(
-      v => v.errors.length === 0
-    );
-    if (hasOffChainVerificationErrors) {
-      console.log(
-        `offChainVerifications: ${JSON.stringify(offChainValidation)}`
-      );
-      return res.status(400).json({
-        errors: offChainValidation
-      });
-    }
-
-    // Verify the on-chain data integrity
-    const txReceiptRetrievalErrors: {
-      layer2Hash: string;
-      error: string;
-    }[] = [];
-    const decodedDataAndLogs: {
-      shareData: IVerifiedData;
-      logs: TDecodedLog[];
-    }[] = [];
-    await Promise.all(
-      shareKitResData.data
-        .filter(d => !isNullOrWhiteSpace(d.tx) && d.tx !== "0x")
-        .map(async shareData => {
-          try {
-            decodedDataAndLogs.push({
-              shareData,
-              logs: await getDecodedTxEventLogs(provider, shareData.tx)
-            });
-          } catch (err) {
-            txReceiptRetrievalErrors.push({
-              layer2Hash: shareData.layer2Hash,
-              error: `${err}`
-            });
-          }
-        })
-    );
-    if (txReceiptRetrievalErrors.length) {
-      console.log(
-        `txReceiptRetrievalErrors: ${JSON.stringify(txReceiptRetrievalErrors)}`
-      );
-      return res.status(400).json({
-        errors: txReceiptRetrievalErrors
-      });
-    }
-
-    if (
-      typeof process.env.VALIDATE_ON_CHAIN === "string" &&
-      process.env.VALIDATE_ON_CHAIN.toLowerCase() === "true"
-    ) {
-      const onChainValidation = validateOnChainProperties(
-        shareKitResData.subject,
-        decodedDataAndLogs
-      );
-      if (onChainValidation.length) {
-        console.log(
-          `onChainVerifications: ${JSON.stringify(onChainValidation)}`
-        );
-        return res.status(400).json({
-          errors: onChainValidation
-        });
-      }
-    }
-
     return res.status(200).json({
       success: true,
       token: req.body.token
